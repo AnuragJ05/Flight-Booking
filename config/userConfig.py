6080a5b20711d3d18a74912e578dd6e993eaaf14
AnuragJ05 <jainanurag424@gmail.com>
2022-10-23 00:05:54 +0530

    Added code for user crud

---
+++ config/userConfig.py
@@ -0,0 +1,53 @@
+import uuid
+from datetime import datetime
+
+import psycopg2
+from flask import request
+from constants.constants import databaseName, dbUser, dbPassword, dbHost, dbPort, adminUserID
+
+
+def get_users():
+    conn = psycopg2.connect(database=databaseName, user=dbUser, password=dbPassword,
+                            host=dbHost, port=dbPort)
+    cur = conn.cursor()  # creating a cursor
+
+    if request.method == "GET":
+        # SELECT "userId", username, "firstName", "middleName", "lastName", "DOB", "mobileNo", "createdBy",
+        # "createdDate", "updatedBy", "updatedDate" FROM dbo."User";
+        cur.execute('SELECT * FROM dbo."User"')
+        rows = cur.fetchall()
+        conn.close()
+        return rows
+
+
+def create_user(data: dict):
+    conn = psycopg2.connect(database=databaseName, user=dbUser, password=dbPassword,
+                            host=dbHost, port=dbPort)
+    cur = conn.cursor()  # creating a cursor
+
+    # INSERT INTO dbo."User"( "userId", username, "firstName", "middleName", "lastName", "DOB", "mobileNo",
+    # "createdBy", "createdDate", "updatedBy", "updatedDate") VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);
+    cur.execute('''INSERT INTO dbo."User"("userId",username, "firstName", "middleName", 
+    "lastName", "DOB", "mobileNo","createdBy","createdDate")
+    VALUES (\'{}\',\'{}\',\'{}\', \'{}\', \'{}\', \'{}\',\'{}\',
+    \'{}\',\'{}\');
+      '''.format(uuid.uuid4(), data["username"], data["firstName"], data["middleName"], data["lastName"],
+                 data["DOB"], data["mobileNo"], adminUserID,
+                 datetime.today().strftime('%d/%m/%y')))
+    conn.commit()
+    conn.close()
+    return data
+
+
+def delete_user(data: dict):
+    conn = psycopg2.connect(database=databaseName, user=dbUser, password=dbPassword,
+                            host=dbHost, port=dbPort)
+    cur = conn.cursor()  # creating a cursor
+
+    # DELETE FROM dbo."User"
+    # 	WHERE <condition>;
+    userName = data["username"]
+    cur.execute('''DELETE FROM dbo."User" WHERE username=\'{}\';'''.format(userName))
+    conn.commit()
+    conn.close()
+    return {"Message": "{} user deleted".format(userName)}
--- main.py
+++ main.py
@@ -1,41 +1,43 @@
-import uuid
+from flask import Flask, request
 
-from flask import Flask, redirect, url_for, request, render_template
-import psycopg2
+from config.userConfig import get_users, create_user, delete_user
 
-# conn = psycopg2.connect(database="testdb", user = "postgres", password = "pass123", host = "127.0.0.1", port = "5432")
-#
-# print "Opened database successfully"
 app = Flask(__name__)
 form_data = None
 
 
-@app.route('/user', methods=["Post", "Get"])
-def get_user():
-    conn = psycopg2.connect(database="Flight-Booking", user="postgres", password="0511", host="127.0.0.1", port="5432")
-
-    print("Opened database successfully")
-    cur = conn.cursor()  # creating a cursor
-
+@app.route('/user', methods=["Get", "Post", "DELETE"])
+def user():
     if request.method == "GET":
-
-        # SELECT "userId", username, "firstName", "middleName", "lastName", "DOB", "mobileNo", "createdBy",
-        # "createdDate", "updatedBy", "updatedDate" FROM dbo."User";
-        cur.execute('SELECT * FROM dbo."User"')
-        rows = cur.fetchall()
-        conn.close()
+        """
+        Get : http://localhost:5050/user
+        """
+        rows = get_users()
         return rows
+
     elif request.method == "POST":
-        # INSERT INTO dbo."User"( "userId", username, "firstName", "middleName", "lastName", "DOB", "mobileNo",
-        # "createdBy", "createdDate", "updatedBy", "updatedDate") VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);
-        cur.execute('''INSERT INTO dbo."User"("userId",username, "firstName", "middleName", 
-        "lastName", "DOB", "mobileNo","createdBy","createdDate")
-        VALUES (\'{}\',\'anurag05\',\'Anurag\', \'Jain\', \'Kumar\', \'1998-11-05\',\'0987654321\',
-        \'a55b99a8-8182-459e-8b67-78abc73d5a06\',\'2022-10-16\');
-          '''.format(uuid.uuid4()))
-        conn.commit()
-        conn.close()
-        return {}
+        """
+        Post : http://localhost:5050/user
+        {
+            "username": "test1234",
+            "firstName": "test2",
+            "middleName": "test2",
+            "lastName": "test2",
+            "DOB": "05/11/2000",
+            "mobileNo": "0999675302"
+        }
+        """
+        data = create_user(data=request.json)
+        return data
+    elif request.method == "DELETE":
+        """
+        Delete : http://localhost:5050/user
+        {
+            "username": "test1234"
+        }
+        """
+        data = delete_user(data=request.json)
+        return data
 
 
 @app.route('/home', methods=["Post"])
@@ -50,4 +52,4 @@ def index():
 
 if __name__ == '__main__':
     # Default 127.0.0.1:5000
-    app.run()
+    app.run(host="localhost", port=5050)
