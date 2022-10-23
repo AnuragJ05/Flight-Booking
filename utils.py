a5008dcf0a13de1f61198af1140da25d0397f8eb
Akshay12061 <116018643+Akshay12061@users.noreply.github.com>
2022-10-23 09:13:33 +0530

    Add files via upload
    adding requirement and utils file

---
+++ utils.py
@@ -0,0 +1,93 @@
+from re import T
+from tkinter.tix import Tree
+from turtle import pd
+import uuid
+import bcrypt
+import psycopg2
+from datetime import datetime
+
+
+def get_db_connection():
+    conn = psycopg2.connect(host='localhost',
+                            database='Flight-Booking',
+                            user= "postgres",
+                            password="0511")
+    conn.autocommit = True
+    return conn
+
+
+def check_passwd(encryp_passwd, passwd):
+    try:
+        if bcrypt.checkpw(encryp_passwd, passwd):
+            return True
+        else:
+            return False
+    except Exception as e:
+        print("Error in check passwd {}".format(e))
+
+
+def insert_new_user(data):
+    try:
+        conn = get_db_connection()
+        cur = conn.cursor()
+
+        uid = uuid.uuid4()
+        insert_query = ('''INSERT INTO dbo."User"
+        ("userId", username, "firstName", "middleName",
+        "lastName", "DOB", "mobileNo", "createdBy", "createdDate")
+        VALUES 
+        ('{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}');'''.format(uid, 
+        data['username'], data['firstName'],
+        data['middleName'], data['lastName'], data['DOB'],
+        data['mobileNo'], uid, datetime.now()))
+        print(insert_query)
+        cur.execute(insert_query)
+        conn.commit()
+
+        p_data = {}
+        p_data["uid"] = uid
+        p_data["password"] = data["password"]
+
+        check = insert_passwd(p_data)
+
+        cur.close()
+        conn.close()
+        return True
+    except (Exception, psycopg2.Error) as error:
+        print("Failed inserting record into mobile table {}".format(error))
+        return False
+
+
+def insert_passwd(data):
+    try:
+        conn = get_db_connection()
+        cur = conn.cursor()
+        pid = uuid.uuid4()
+        p = data['password']
+        encryt_p = bcrypt.hashpw(p, bcrypt.gensalt()) 
+        insert_query = ('''INSERT INTO dbo."UserPasswordEntity"
+        ("userPasswordId", userId, "passwordHash", "otpSecret", "createdDate")
+        VALUES 
+        ('{}', '{}', '{}', '{}', '{}');'''.format(pid, 
+        data['uid'], encryt_p,
+        data['middleName'], '', datetime.now()))
+        print(insert_query)
+        cur.execute(insert_query)
+        conn.commit()
+        cur.close()
+        conn.close()
+        return True
+    except (Exception, psycopg2.Error) as error:
+        print("Failed inserting record into mobile table {}".format(error))
+        return False
+
+
+
+# data = {}
+# data['username'] = "akshay@bits.com" 
+# data['firstName'] = "Akshay"
+# data['middleName'] = "S"
+# data['lastName'] = "Bhavsar"
+# data['DOB'] = "06/03/1993"
+# data['mobileNo'] = 8237355846 
+# datetime.now()
